4c480de22696b2ef1a2ed4dbe13051ca472435ea
jubskan3ki <93186899+jubskan3ki@users.noreply.github.com>
2023-07-31 13:48:22 +0200

    Update Responsive

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { NavLink,  useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse ,faRightFromBracket , faTrophy ,faStar } from '@fortawesome/free-solid-svg-icons';
+import { useMediaQuery } from 'react-responsive';
 import './Navbar.css';
+
 
 const NavBar: React.FC = () => {
     const navigate = useNavigate();
+    const isMobile = useMediaQuery({ maxWidth: 800 });
 
     const handleLogout = () => {
         localStorage.removeItem('authToken');
@@ -15,19 +18,35 @@ const NavBar: React.FC = () => {
 
     return (
         <div className="Navbar">
-            <div>
-                <NavLink to="/Home" className={({ isActive }) => (isActive ? 'selected' : '')} >
-                    <FontAwesomeIcon icon={faHouse} />
-                </NavLink>
+            {!isMobile ? (
+                <div className="nav-links">
+                    <NavLink to="/Home" className={({ isActive }) => (isActive ? 'selected' : '')} >
+                        <FontAwesomeIcon icon={faHouse} />
+                    </NavLink>
 
-                <NavLink to="/Ranking" className={({ isActive }) => (isActive ? 'selected' : '')}>
-                    <FontAwesomeIcon icon={faTrophy} />
-                </NavLink>
+                    <NavLink to="/Ranking" className={({ isActive }) => (isActive ? 'selected' : '')}>
+                        <FontAwesomeIcon icon={faTrophy} />
+                    </NavLink>
 
-                <NavLink to="/Score" className={({ isActive }) => (isActive ? 'selected' : '')}>
-                    <FontAwesomeIcon icon={faStar} />
-                </NavLink>
-            </div>
+                    <NavLink to="/Score" className={({ isActive }) => (isActive ? 'selected' : '')}>
+                        <FontAwesomeIcon icon={faStar} />
+                    </NavLink>
+                </div>
+            ) : (
+                <>
+                    <NavLink to="/Home" className={({ isActive }) => (isActive ? 'selected' : '')} >
+                        <FontAwesomeIcon icon={faHouse} />
+                    </NavLink>
+
+                    <NavLink to="/Ranking" className={({ isActive }) => (isActive ? 'selected' : '')}>
+                        <FontAwesomeIcon icon={faTrophy} />
+                    </NavLink>
+
+                    <NavLink to="/Score" className={({ isActive }) => (isActive ? 'selected' : '')}>
+                        <FontAwesomeIcon icon={faStar} />
+                    </NavLink>
+                </>
+            )}
 
             <div onClick={handleLogout} className='selected'>
                 <FontAwesomeIcon icon={faRightFromBracket} />
